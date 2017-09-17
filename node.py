c78c3eccbd28ccd5cbadf4a3e820d6e2f730873b
Matt Vogel <matt@portlandwebdev.com>
2017-09-16 23:42:02 -0700

    add friend requesting

--- node.py
+++ node.py
@@ -4,9 +4,14 @@ import json
 import requests
 import time
 from uuid import uuid4
+from ecdsa import SigningKey
 
+with open('config.json') as f:
+    config = json.loads(f.read())
 
-def generateBlock(blocks, coinbase, block_reward, friend_requests, friend_accepts):
+sk = SigningKey.from_string(config.get('private_key').decode('hex'))
+
+def generate_block(blocks, coinbase, block_reward, friend_requests, friend_accepts):
     block = {
         'index': len(blocks),
         'prevHash': blocks[len(blocks)-1]['hash'] if len(blocks) > 0 else '',
@@ -28,22 +33,71 @@ if __name__ == "__main__":
                     help='If you want to mine blocks')
     args = parser.parse_args()
     if args.runtype == 'node':
-        # first thing we'll do is search for friend requests
-        with open('blockchain.json', 'r') as f:
-            blocks = json.loads(f.read())['blocks']
+        block_height = 0
+        friends_list = []
+        while 1:
+            # first thing we'll do is search for friend requests
+            with open('blockchain.json', 'r') as f:
+                blocks = json.loads(f.read())['blocks']
 
-        for block in blocks:
+            for i, block in enumerate(blocks):
+                if block_height >= i:
+                    continue
+                block_height = i
+                for friend_request in block.get('friend_requests'):
+                    sig = sk.sign_deterministic(friend_request['to'])
+                    if friend_request['rid'] == sig.encode('hex'):
+                        print 'FOUND'
+                        with open('friend_requests.json', 'a+') as f:
+                            try:
+                                existing = json.loads(f.read())
+                            except:
+                                existing = []
+                            existing.append(friend_request)
+                            f.seek(0)
+                            f.truncate()
+                            friend_requests = dict([(c['rid'], c) for c in existing])
+                            f.write(json.dumps([c for e, c in friend_requests.iteritems()], indent=4))
+                            f.truncate()
+                    else:
+                        print 'NOT FOUND'
+                        print friend_request['rid']
+                        print friend_request['to']
+                        print sk.sign(friend_request['to']).encode('hex')
+                        print sk.get_verifying_key().to_string().encode('hex')
 
+                for friend_accept in block.get('friend_accepts'):
+                    sig = sk.sign_deterministic(friend_accept['from'])
+                    if friend_request['rid'] == sig.encode('hex'):
+                        print 'FOUND'
+                        with open('friend_requests.json', 'a+') as f:
+                            try:
+                                existing = json.loads(f.read())
+                            except:
+                                existing = []
+                            existing.append(friend_request)
+                            f.seek(0)
+                            f.truncate()
+                            friend_requests = dict([(c['rid'], c) for c in existing])
+                            f.write(json.dumps([c for e, c in friend_requests.iteritems()], indent=4))
+                            f.truncate()
+                    else:
+                        print 'NOT FOUND'
+                        print friend_request['rid']
+                        print friend_request['to']
+                        print sk.sign(friend_request['to']).encode('hex')
+                        print sk.get_verifying_key().to_string().encode('hex')
+
+            time.sleep(1)
     elif args.runtype == 'mine':
 
         # default run state will be to mine some blocks!
 
         # proof of work time!
+        ii = 0
         while 1:
-            with open('config.json') as f:
-                config = json.loads(f.read())
             coinbase = config.get('coinbase')
-            block_reward = config.get('coinbase')
+            block_reward = config.get('block_reward')
             difficulty = config.get('difficulty')
 
             with open('blockchain.json') as f:
@@ -65,22 +119,24 @@ if __name__ == "__main__":
                     f.truncate()
 
             if not friend_requests and not friend_accepts and len(blocks):
-                print 'not genesis and no transactions'
+                print 'not genesis and no transactions. Idle time: %s\r' % ii
+                ii += 1
                 time.sleep(1)
                 continue
-            latest_block = blocks[len(blocks)-1] if len(blocks) > 1 else generateBlock([], coinbase, block_reward, [], [])
+
+            latest_block = blocks[len(blocks)-1] if len(blocks) > 1 else generate_block([], coinbase, block_reward, [], [])
             nonce = latest_block.get('nonce')
-            x = 0
+            i = 0
             while 1:
-                hash_test = hashlib.sha256("%s%s" % (nonce, x)).digest().encode('hex')
+                hash_test = hashlib.sha256("%s%s" % (nonce, i)).digest().encode('hex')
                 print hash_test
                 if hash_test.endswith(difficulty):
                     print 'got a block!'
-                    print 'verify answer, nonce: ', nonce, 'interation: ', x, 'hash: ', hashlib.sha256("%s%s" % (nonce, x)).hexdigest()
+                    print 'verify answer, nonce: ', nonce, 'interation: ', i, 'hash: ', hashlib.sha256("%s%s" % (nonce, i)).hexdigest()
                     # create the block with the reward
                     # gather friend requests from the network
 
-                    block = generateBlock(blocks, coinbase, block_reward, friend_requests, friend_accepts)
+                    block = generate_block(blocks, coinbase, block_reward, friend_requests, friend_accepts)
 
                     with open('blockchain.json', 'r+') as f:
                         blocks = json.loads(f.read()).get('blocks')
@@ -100,7 +156,6 @@ if __name__ == "__main__":
                         )
                         print res.content
                     break
-                x += 1
-            print 'block not found in ', x, ' iterations'
-
+                i += 1
+            ii = 0
             time.sleep(1)
--- rpc.py
+++ rpc.py
@@ -1,27 +1,38 @@
 import json
+import hashlib
+import os
 
 from flask import Flask, request
-from ecdsa import SigningKey, VerifyingKey
+from ecdsa import NIST384p, SigningKey
+from ecdsa.util import randrange_from_seed__trytryagain
 
-key = '841984251447c81c6374d17102abb9104278cb9cc9886934'
-sk = SigningKey.from_string(key.decode('hex'))
-#vk2 = VerifyingKey.from_string(pk.decode('hex'))
-#print vk2.verify(signature, "message")
+
+with open('config.json') as f:
+    config = json.loads(f.read())
+
+key = config.get('private_key').decode('hex')
+# print sk.get_verifying_key().to_string().encode('hex')
+# vk2 = VerifyingKey.from_string(pk.decode('hex'))
+# print vk2.verify(signature, "message")
 
 app = Flask(__name__)
+
 
 @app.route('/get-block/<index>')
 def get_block(index=None):
     return json.dumps({'hi':index})
 
+
 @app.route('/get-latest-block')
 def get_latest_block():
     return json.dumps({'hi':'latest block'})
+
 
 @app.route('/get-chain')
 def get_chain():
     # some type of generator
     return json.dumps()
+
 
 @app.route('/get-peers')
 def get_peers():
@@ -29,16 +40,20 @@ def get_peers():
         peers = f.read()
     return json.dumps({'peers': peers})
 
+
 @app.route('/post-block', methods=['POST'])
 def post_block():
     print request.content_type
     print request.get_json()
     return json.dumps(request.get_json())
 
+
 @app.route('/friend-request', methods=['POST'])
 def friend_request():
     # add it to friend request pool to be included in a block
-    signature = sk.sign(request.form.get('to'))
+
+    sk = SigningKey.from_string(key)
+    signature = sk.sign_deterministic(request.form.get('to'))  # indexer reference baked into request's "return signature"
     friend_request = {
         'to': request.form.get('to'),
         'rid': signature.encode('hex')
@@ -51,6 +66,7 @@ def friend_request():
         f.truncate()
 
     return json.dumps(existing)
+
 
 @app.route('/friend-accept', methods=['POST'])
 def friend_accept():
@@ -68,6 +84,7 @@ def friend_accept():
         f.truncate()
 
     return json.dumps(existing)
+
 
 @app.route('/friend-request-search', methods=['POST'])
 def friend_request_search():
