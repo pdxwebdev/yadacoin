0525c47480fc36f210afd6b3ca930478ad034fe6
Matt Vogel <matt@portlandwebdev.com>
2017-10-12 19:21:57 -0700

    fix str and unicode conflict

--- rpc.py
+++ rpc.py
@@ -55,7 +55,7 @@ class TransacationFactory(object):
         my_bulletin_secret = TU.generate_deterministic_signature()
         if my_bulletin_secret == self.bulletin_secret:
             raise BaseException('bulletin secrets are identical. do you love yourself so much that you want a relationship on the blockchain?')
-        rids = sorted([my_bulletin_secret, self.bulletin_secret], key=str.lower)
+        rids = sorted([str(my_bulletin_secret), str(self.bulletin_secret)], key=str.lower)
         return hashlib.sha256(rids[0] + rids[1]).digest().encode('hex')
 
     def generate_relationship(self):
