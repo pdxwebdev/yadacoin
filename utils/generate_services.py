9eb1ed167c6f30f37b98b8997e84adc12fc16652
Matt Vogel <matt@portlandwebdev.com>
2018-09-18 13:27:51 -0700

    adding service generator

---
+++ utils/generate_services.py
@@ -0,0 +1,45 @@
+import os
+if not os.path.exists(os.getcwd() + '/services'):
+    os.makedirs(os.getcwd() + '/services')
+with open(os.getcwd() + '/services/yadacoin-serve.service', 'w+') as f:
+    out = """#!/bin/bash
+[Unit]
+Description=Yada Coin Serve Worker
+
+[Service]
+Type=simple
+WorkingDirectory={cwd}
+ExecStart={cwd}scripts/start_serve.sh
+
+[Install]
+WantedBy=multi-user.target
+""".format(cwd=os.getcwd())
+    f.write(out)
+with open(os.getcwd() + '/services/yadacoin-mine.service', 'w+') as f:
+    out = """#!/bin/bash
+[Unit]
+Description=Yada Coin Mine Worker
+
+[Service]
+Type=simple
+WorkingDirectory={cwd}
+ExecStart={cwd}scripts/start_mine.sh
+
+[Install]
+WantedBy=multi-user.target
+""".format(cwd=os.getcwd())
+    f.write(out)
+with open(os.getcwd() + '/services/yadacoin-consensus.service', 'w+') as f:
+    out = """#!/bin/bash
+[Unit]
+Description=Yada Coin Consensus Worker
+
+[Service]
+Type=simple
+WorkingDirectory={cwd}
+ExecStart={cwd}scripts/start_consensus.sh
+
+[Install]
+WantedBy=multi-user.target
+""".format(cwd=os.getcwd())
+    f.write(out)
