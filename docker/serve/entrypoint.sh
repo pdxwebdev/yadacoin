15320a80c517c51867d11f351c3589448482b453
Matt Vogel <matt@portlandwebdev.com>
2018-10-12 21:33:15 -0700

    docker changes

--- docker/serve/entrypoint.sh
+++ docker/serve/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -u /usr/src/app/yadacoin/utils/generate_config.py auto -c config-docker.json
-python /usr/src/app/yadacoin/p2p.py serve config-docker.json
+python -u /usr/src/app/yadacoin/utils/generate_config.py auto -c /config/config-docker.json
+python /usr/src/app/yadacoin/p2p.py serve /config/config-docker.json
