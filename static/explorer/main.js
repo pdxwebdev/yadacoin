760ea4cd0100dd8da3c073f125e084edc76dabb8
Matt Vogel <matt@portlandwebdev.com>
2018-11-08 10:42:32 -0800

    update explorer

--- static/explorer/main.js
+++ static/explorer/main.js
@@ -193,11 +193,15 @@ var SearchFormComponent = /** @class */ (function () {
         this.submitted = false;
         this.current_height = '';
         this.circulating = '';
+        this.hashrate = '';
         this.http.get('/get-latest-block')
             .subscribe(function (res) {
             _this.current_height = Number(res.json().index + 1).toLocaleString();
             _this.result = [res.json()];
             _this.circulating = Number(res.json().index * 50).toLocaleString();
+            var blocks_found = res.json().index;
+            var difficulty = parseInt('00000000FFFF0000000000000000000000000000000000000000000000000000', 16) / parseInt(res.json().target, 16);
+            _this.hashrate = Number(blocks_found / 144 * difficulty * Math.pow(2, 32) / 600).toLocaleString() + '/Hs;';
         }, function (err) {
             alert('something went terribly wrong!');
         });
