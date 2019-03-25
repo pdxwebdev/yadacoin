5d669a0b70f92ea3084bd7b862cd7788ef485e84
EggPool <dev@eggpool.net>
2019-03-25 19:30:04 +0100

    Port send.py to python3

--- yadacoin/send.py
+++ yadacoin/send.py
@@ -1,14 +1,15 @@
 from socketIO_client import SocketIO, BaseNamespace
 
-from peers import Peers
-from transaction import TransactionFactory, Output, NotEnoughMoneyException
-from transactionutils import TU
-from blockchainutils import BU
+from yadacoin.peers import Peers
+from yadacoin.transaction import TransactionFactory, Output, NotEnoughMoneyException
+from yadacoin.transactionutils import TU
+from yadacoin.blockchainutils import BU
 
 
 class ChatNamespace(BaseNamespace):
     def on_error(self, event, *args):
-        print 'error'
+        print('error')
+
 
 class Send(object):
     @classmethod
@@ -28,22 +29,22 @@ class Send(object):
                 ]
             )
         except NotEnoughMoneyException as e:
-            print "not enough money yet"
+            print("not enough money yet")
             return
         except:
             raise
         try:
             transaction.transaction.verify()
         except:
-            print 'transaction failed'
+            print('transaction failed')
         TU.save(config, mongo, transaction.transaction)
-        print 'Transaction generated successfully. Sending:', value, 'To:', to 
+        print('Transaction generated successfully. Sending:', value, 'To:', to)
         for peer in Peers.peers:
             try:
                 with SocketIO(peer.host, peer.port, ChatNamespace, wait_for_connection=False) as socketIO:
                     chat_namespace = socketIO.define(ChatNamespace, '/chat')
                     chat_namespace.emit('newtransaction', transaction.transaction.to_dict())
                     socketIO.disconnect()
-                    print 'Sent to:', peer.host, peer.port
+                    print('Sent to:', peer.host, peer.port)
             except Exception as e:
-                print e
+                print(e)
