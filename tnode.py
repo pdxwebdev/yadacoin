03ec0fa4df521633c5182c9b4d25c9cc390ac41a
EggPool <dev@eggpool.net>
2019-03-26 21:57:26 +0100

    First scaffold of integrated server+consensus - Not functional

---
+++ tnode.py
@@ -0,0 +1,70 @@
+"""
+Async Yadacoin node poc
+"""
+
+from os import path
+# import tornado.web
+from tornado.web import Application, RequestHandler, StaticFileHandler
+from tornado.options import define, options
+import tornado.ioloop
+import tornado.locks
+
+from yadacoin.basehandlers import BaseHandler
+
+
+__version__ = '0.0.1'
+
+define("debug", default=False, help="debug mode", type=bool)
+define("verbose", default=False, help="verbose mode", type=bool)
+# TODO: get from config file
+define("port", default=8000, help="run on the given port", type=int)
+define("listen", default="127.0.0.1", help="On which address to listen, locked by default to localhost for tests", type=str)
+
+
+class NodeApplication(Application):
+
+    def __init__(self):
+        static_path = path.join(options.theme, 'static')
+        self.default_handlers = [
+            (r"/", HomeHandler),
+            (r"/(apple-touch-icon\.png)", StaticFileHandler,
+             dict(path=static_path))
+        ]
+
+        settings = dict(
+            app_title=u"Yadacoin Node",
+            template_path=path.join(path.dirname(__file__), 'templates'),
+            static_path=path.join(path.dirname(__file__), static_path),
+            xsrf_cookies=True,
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            compress_response=True,
+            debug=options.debug,  # Also activates auto reload
+            serve_traceback=options.debug,
+            yadacoin_vars = {'node_version': __version__},
+        )
+        handlers = self.default_handlers.copy()
+        super(Application, self).__init__(handlers, **settings)
+
+
+class HomeHandler(BaseHandler):
+
+    async def get(self):
+        """
+        :return:
+        """
+        self.render("home.html", yadacoin=self.yadacoin_vars)
+
+
+async def main():
+    tornado.options.parse_command_line()
+    app = NodeApplication()
+    app.listen(options.port, options.listen)
+    # The server will simply run until interrupted
+    # with Ctrl-C, but if you want to shut down more gracefully,
+    # call shutdown_event.set().
+    shutdown_event = tornado.locks.Event()
+    await shutdown_event.wait()
+
+
+if __name__ == "__main__":
+        tornado.ioloop.IOLoop.current().run_sync(main)
---
+++ yadacoin/basehandlers.py
@@ -0,0 +1,42 @@
+"""
+Base handler ancestor, factorize common functions
+"""
+
+import logging
+from tornado.web import RequestHandler
+
+
+class BaseHandler(RequestHandler):
+    """Common ancestor for all route handlers"""
+
+    def initialize(self):
+        """Common init for every request"""
+        self.app_log = logging.getLogger("tornado.application")
+        self.yadacoin_config = self.settings['yadacoin_config']
+        self.yadacoin_vars = self.settings['yadacoin_vars']
+        self.settings["page_title"] = self.settings["app_title"]
+
+    # This could be static, but its easier to let it there so the template have direct access.
+    def bool2str(self, a_boolean, iftrue, iffalse):
+        return iftrue if a_boolean else iffalse
+
+    def active_if(self, path: str):
+        """return the 'active' string if the request uri is the one in path. Used for menu css"""
+        if self.request.uri == path:
+            return "active"
+        return ''
+
+    def active_if_start(self, path: str):
+        """return the 'active' string if the request uri begins with the one in path. Used for menu css"""
+        if self.request.uri.startswith(path):
+            return "active"
+        return ''
+
+    def checked_if(self, condition:bool):
+        if condition:
+            return "checked"
+        return ''
+
+    def message(self, title, message, type="info"):
+        """Display message template page"""
+        self.render("message.html", yadacoin=self.yadacoin_vars, title=title, message=message, type=type)
