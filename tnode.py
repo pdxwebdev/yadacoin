95010bb78f81a3f2fccd2ecb9e451a3166667583
EggPool <dev@eggpool.net>
2019-03-27 11:28:52 +0100

    Integrate consensus syncing

--- tnode.py
+++ tnode.py
@@ -11,30 +11,27 @@ from tornado.options import define, options
 import tornado.ioloop
 import tornado.locks
 from sys import exit
+from asyncio import sleep as async_sleep
 
 from yadacoin.config import Config
 from yadacoin.basehandlers import BaseHandler
 from yadacoin.corehandlers import GetLatestBlockHandler, GetBlocksHandler, GetBlockHandler
+from yadacoin.consensus import Consensus
 from yadacoin.mongo import Mongo
 
 
-__version__ = '0.0.2'
+__version__ = '0.0.4'
 
 define("debug", default=False, help="debug mode", type=bool)
 define("verbose", default=False, help="verbose mode", type=bool)
 define("network", default='mainnet', help="mainnet, testnet or regnet", type=str)
 define("reset", default=False, help="If blockchain is invalid, truncate at error block", type=bool)
 define("config", default='config/config.json', help="Config file location, default is 'config/config.json'", type=str)
-
-
-# TODO: get from config file
-define("port", default=8000, help="run on the given port", type=int)
-define("listen", default="127.0.0.1", help="On which address to listen, locked by default to localhost for tests", type=str)
-
+define("verify", default=True, help="Verify chain, default True", type=bool)
 
 class NodeApplication(Application):
 
-    def __init__(self, config):
+    def __init__(self, config, mongo):
         static_path = path.join(path.dirname(__file__), 'static')
         self.default_handlers = [
             (r"/", HomeHandler),
@@ -55,7 +52,7 @@ class NodeApplication(Application):
             serve_traceback=options.debug,
             yadacoin_vars={'node_version': __version__},
             yadacoin_config=config,
-            mongo=Mongo(config)
+            mongo=mongo
         )
         handlers = self.default_handlers.copy()
         super().__init__(handlers, **settings)
@@ -70,6 +67,13 @@ class HomeHandler(BaseHandler):
         self.render("index.html", yadacoin=self.yadacoin_vars)
 
 
+async def background_consensus(consensus):
+    while True:
+        wait = consensus.sync_bottom_up()
+        if wait:
+            await async_sleep(1)
+
+
 async def main():
     tornado.options.parse_command_line()
     if path.isfile(options.config):
@@ -78,7 +82,16 @@ async def main():
     else:
         print("no config file found at '%s'" % options.config)
         exit()
-    app = NodeApplication(config)
+
+    mongo = Mongo(config)
+
+    consensus = Consensus(config, mongo, options.debug)
+    if config.verify:
+        logging.getLogger("tornado.application").info("Verifying existing blockchain".format(config.serve_host, config.serve_port))
+        consensus.verify_existing_blockchain(reset=options.reset)
+    tornado.ioloop.IOLoop.instance().add_callback(background_consensus, consensus)
+
+    app = NodeApplication(config, mongo)
     logging.getLogger("tornado.application").info("Starting server on {}:{}".format(config.serve_host, config.serve_port))
     app.listen(config.serve_port, config.serve_host)
     # The server will simply run until interrupted
