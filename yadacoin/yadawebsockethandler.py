8f42bd038e54daf562fc3decdd11e753b094d03b
EggPool <dev@eggpool.net>
2019-04-07 10:50:10 +0200

    Converted websocket handlers to a Class

--- yadacoin/yadawebsockethandler.py
+++ yadacoin/yadawebsockethandler.py
@@ -9,15 +9,59 @@ from yadacoin.transaction import Transaction
 from yadacoin.config import get_config
 from yadacoin.blockchainutils import BU
 
+
+class ChatNamespace(socketio.AsyncNamespace):
+
+    def on_connect(self, sid, environ):
+        pass
+        print('Client connected')
+        # await SIO.emit('my response', {'data': 'Connected', 'count': 0}, room=sid, namespace='/chat')
+
+    async def on_disconnect_request(sid):
+        # print('Disconnect request')
+        await SIO.disconnect(sid, namespace='/chat')
+
+    def on_disconnect(self, sid):
+        pass
+        print('Client disconnected')
+
+    async def on_newtransaction(self, sid, data):
+        print("newtransaction", data)
+        try:
+            incoming_txn = Transaction.from_dict(BU().get_latest_block()['index'], data)
+        except Exception as e:
+            print("transaction is bad", e)
+            raise Exception("transaction is bad")
+
+        try:
+            print(incoming_txn.transaction_signature)
+            dup_check = get_config().mongo.db.miner_transactions.find({'id': incoming_txn.transaction_signature})
+            if dup_check.count():
+                print('found duplicate')
+                raise Exception("duplicate")
+                get_config().mongo.db.miner_transactions.update(incoming_txn.to_dict(), incoming_txn.to_dict(),
+                                                                upsert=True)
+        except Exception as e:
+            raise Exception("transaction is bad", e)
+
+    async def on_hello(self, sid, data):
+        print("hello", data)
+        try:
+            pass
+        except Exception as e:
+            raise Exception("bad hello")
+
 SIO = socketio.AsyncServer(async_mode='tornado')
 # see https://github.com/miguelgrinberg/python-socketio/blob/master/examples/server/tornado/app.py
+SIO.register_namespace(ChatNamespace('/chat'))
+# See https://python-socketio.readthedocs.io/en/latest/server.html#namespaces
 
 # TODO: refactor as a class or use a global CONFIG var from yadacoin.config, would avoid passing config as param everywhere
 # Same goes for mongo
 #WS_CONFIG = None
 #WS_MONGO = None
 
-
+"""
 @SIO.on('connect', namespace='/chat')
 async def chat_connect(sid, environ):
     pass
@@ -63,6 +107,10 @@ async def newtransaction(sid, data):
         pass
     except Exception as e:
         raise Exception("bad hello")
+
+"""
+
+
 
 # no newblock websocket event seems used
 """
