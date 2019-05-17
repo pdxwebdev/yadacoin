c25be10ccd5ca989b96eae1b308fe27d0563a6df
EggPool <33421807+EggPool@users.noreply.github.com>
2019-05-17 20:10:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,28 @@
+# Install MongoDB
 sudo mkdir /data/db -p
 sudo chmod 777 /data/db
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-sudo echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 sudo apt update
 sudo apt install -y mongodb-org
+# Setup and start DB service
 sudo systemctl enable mongod.service
 sudo systemctl start mongod.service
-sudo apt install -y libxml2-dev libxmlsec1-dev python-dev build-essential
+
+# sudo apt install -y libxml2-dev libxmlsec1-dev python-dev build-essential
+
+# Get and clone repo
 cd ~
 git clone https://github.com/pdxwebdev/yadacoin.git
 cd yadacoin
-sudo apt install -y python-pip
-sudo pip install virtualenv
-virtualenv venv
-venv/bin/pip install -r requirements.txt
-venv/bin/python utils/generate_config.py auto > config/config.json
-venv/bin/python utils/generate_services.py
+# Install prerequisites
+sudo apt install -y python3-pip
+pip3 install -r requirements-python3.txt
+
+python3 utils/generate_config.py auto > config/config.json
+# TODO: config needs more editing (ip?)
+python3 utils/generate_services.py
 sudo cp services/* /lib/systemd/system/.
 sudo systemctl daemon-reload
-sudo systemctl enable yadacoin-serve
-sudo systemctl enable yadacoin-mine
-sudo systemctl enable yadacoin-consensus
-sudo service yadacoin-mine start
-sudo service yadacoin-consensus start
-sudo service yadacoin-serve start
+sudo systemctl enable yadacoin-node
+# sudo service yadacoin-node start
