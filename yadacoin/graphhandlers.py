152356a6a879a881ce146820186415163318302c
Matt Vogel <matt@portlandwebdev.com>
2019-05-18 13:16:33 -0700

    refactor push notifications

--- yadacoin/graphhandlers.py
+++ yadacoin/graphhandlers.py
@@ -162,7 +162,7 @@ class GraphTransactionHandler(BaseGraphHandler):
         for x in transactions:
             await self.config.mongo.async_db.miner_transactions.insert_one(x.to_dict())
             try:
-                self.do_push(x.to_dict(), self.bulletin_secret)
+                self.config.push_service.do_push(x.to_dict(), self.bulletin_secret)
             except:
                 print('do_push failed')
         """
@@ -172,86 +172,6 @@ class GraphTransactionHandler(BaseGraphHandler):
         job.start()
         """
         self.render_as_json(items)
-
-    async def do_push(self, txn, bulletin_secret):
-        mongo = self.config.mongo
-        my_bulletin_secret = self.config.bulletin_secret
-        rids = sorted([str(my_bulletin_secret), str(bulletin_secret)], key=str.lower)
-        rid = hashlib.sha256(str(rids[0]) + str(rids[1])).digest().encode('hex')
-
-        rids = self.config.GU.get_mutual_rids(rid)
-        username_txns = [x for x in self.config.GU.search_rid(rids)]
-        if username_txns:
-            username = username_txns[0]['relationship']['their_username']
-
-        if txn.get('relationship') and txn.get('dh_public_key') and txn.get('requester_rid') == rid:
-            #friend request
-            #if rid is the requester_rid, then we send a friend request notification to the requested_rid
-            res = await mongo.async_site_db.fcmtokens.find({"rid": txn['requested_rid']})
-            for token in res:
-                result = self.config.push_service.notify_single_device(
-                    registration_id=token['token'],
-                    message_title='%s sent you a friend request!' % username,
-                    message_body="See the request and approve!",
-                    extra_kwargs={'priority': 'high'}
-                )
-
-        elif txn.get('relationship') and txn.get('dh_public_key') and txn.get('requested_rid') == rid:
-            #friend accept
-            #if rid is the requested_rid, then we send a friend accepted notification to the requester_rid
-            res = await mongo.async_mongo.site_db.fcmtokens.find({"rid": txn['requester_rid']})
-            for token in res:
-                result = self.config.push_service.notify_single_device(
-                    registration_id=token['token'],
-                    message_title='%s approved your friend request!' % username,
-                    message_body='Say "hi" to your friend!',
-                    extra_kwargs={'priority': 'high'}
-                )
-
-        elif txn.get('relationship') and not txn.get('dh_public_key') and not txn.get('rid'):
-            #post
-            #we find all mutual friends of rid and send new post notifications to them
-            rids = []
-            rids.extend([x['requested_rid'] for x in self.config.BU.get_sent_friend_requests(rid)])
-            rids.extend([x['requester_rid'] for x in self.config.BU.get_friend_requests(rid)])
-            for friend_rid in rids:
-                res = await mongo.async_mongo.site_db.fcmtokens.find({"rid": friend_rid})
-                used_tokens = []
-                for token in res:
-                    if token['token'] in used_tokens:
-                        continue
-                    used_tokens.append(token['token'])
-
-                    result = self.config.push_service.notify_single_device(
-                        registration_id=token['token'],
-                        message_title='%s has posted something!' % username,
-                        message_body='Check out what your friend posted!',
-                        extra_kwargs={'priority': 'high'}
-                    )
-
-        elif txn.get('relationship') and not txn.get('dh_public_key') and txn.get('rid'):
-            #message
-            #we find the relationship of the transaction rid and send a new message notification to the rid
-            #of the relationship that does not match the arg rid
-            txns = [x for x in self.config.BU.get_transactions_by_rid(txn['rid'], self.config.bulletin_secret, rid=True, raw=True)]
-            rids = []
-            rids.extend([x['requested_rid'] for x in txns if 'requested_rid' in x and rid != x['requested_rid']])
-            rids.extend([x['requester_rid'] for x in txns if 'requester_rid' in x and rid != x['requester_rid']])
-            for friend_rid in rids:
-                res = await mongo.async_mongo.site_db.fcmtokens.find({"rid": friend_rid})
-                used_tokens = []
-                for token in res:
-                    if token['token'] in used_tokens:
-                        continue
-                    used_tokens.append(token['token'])
-
-                    result = self.config.push_service.notify_single_device(
-                        registration_id=token['token'],
-                        message_title='New message from %s!' % username,
-                        message_body='Go see what your friend said!',
-                        extra_kwargs={'priority': 'high'}
-                    )
-                    print(result)
 
 
 class CreateRelationshipHandler(BaseHandler):
@@ -519,10 +439,11 @@ class SignRawTransactionHandler(BaseHandler):
             })
 
 
-class FastGraphHandler(BaseHandler):
+class FastGraphHandler(BaseGraphHandler):
     async def post(self):
         # after the necessary signatures are gathered, the transaction is sent here.
         mongo = self.config.mongo
+        self.get_base_graph()
         fastgraph = json.loads(self.request.body.decode('utf-8'))
         fastgraph = FastGraph.from_dict(0, fastgraph)
         try:
@@ -544,6 +465,10 @@ class FastGraphHandler(BaseHandler):
         # TODO: use new peer framework to broadcast fastgraph transactions
         #fastgraph.broadcast()
         self.render_as_json(fastgraph.to_dict())
+        try:
+            self.config.push_service.do_push(fastgraph.to_dict(), self.bulletin_secret)
+        except:
+            print('do_push failed')
 
 
 # these routes are placed in the order of operations for getting started.
