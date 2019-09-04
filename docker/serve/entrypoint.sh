581aeda912bd46cf5bc42f2042acaa01fce13f8a
Matt Vogel <matt@portlandwebdev.com>
2019-09-03 22:34:19 -0700

    update docker files

--- docker/serve/entrypoint.sh
+++ docker/serve/entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-python -u /usr/src/app/yadacoin/utils/generate_config.py auto -c /config/config-docker.json --mongo-host mongodb
-python /usr/src/app/yadacoin/p2p.py serve /config/config-docker.json
+python /usr/src/app/yadacoin/tnode.py
