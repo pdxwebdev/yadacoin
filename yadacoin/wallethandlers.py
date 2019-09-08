509b975c89d9c43871d0afb989bc6564269f9bf9
Matt Vogel <matt@portlandwebdev.com>
2019-09-08 02:11:08 -0700

    enhance create child wallet endpoint

--- yadacoin/wallethandlers.py
+++ yadacoin/wallethandlers.py
@@ -2,7 +2,8 @@
 Handlers required by the wallet operations
 """
 
-
+from bip32utils import BIP32Key
+from bitcoin.wallet import CBitcoinSecret, P2PKHBitcoinAddress
 from yadacoin.basehandlers import BaseHandler
 
 
@@ -20,8 +21,26 @@ class GenerateWalletHandler(BaseHandler):
 
 class GenerateChildWalletHandler(BaseHandler):
 
-    async def get(self):
-        return self.render_as_json({"address": "qlkwerjqlwekrjqwlerkjlwekrj"})
+    async def post(self):
+        exkey = BIP32Key.fromExtendedKey(self.config.xprv)
+        last_child_key = self.config.async_db.child_keys.find_one({'account': self.get_body_argument('account')}).sort([('inc', -1)])
+        if last_child_key:
+            inc = last_child_key['inc'] + 1
+        else:
+            inc = 0
+        key = exkey.ChildKey(inc)
+        child_key = BIP32Key.fromExtendedKey(key.ExtendedKey())
+        private_key = child_key.PrivateKey().hex()
+        public_key = child_key.PublicKey().hex()
+        address = str(P2PKHBitcoinAddress.from_pubkey(bytes.fromhex(public_key)))
+        await self.config.async_db.child_keys.insert_one({
+            'account': self.get_body_argument('account'),
+            'inc': inc,
+            'extended': child_key.ExtendedKey(),
+            'public_key': private_key,
+            'address': address
+        })
+        return self.render_as_json({"address": address})
 
 
 WALLET_HANDLERS = [(r'/wallet', WalletHandler), (r'/generate-wallet', GenerateWalletHandler),
