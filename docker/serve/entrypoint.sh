362361f09f7604e7eacbdaeb11f686db464b1d97
Matt Vogel <matt@portlandwebdev.com>
2020-01-05 13:31:24 -0800

    fix docker

--- docker/serve/entrypoint.sh
+++ docker/serve/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir /config
 python -u /usr/src/app/yadacoin/utils/generate_config.py auto -c /config/config-docker.json --mongodb-host mongodb
 python /usr/src/app/yadacoin/tnode.py --config=/config/config-docker.json --debug=true --verify=false
