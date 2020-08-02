ab5a7b7023b7b20ece877d120cdf7d55687a3cd3
Matt Vogel <matt@portlandwebdev.com>
2020-08-02 10:23:25 -0700

    remove old endpoints and files

--- yadacoin/poolhandlers.py
+++ yadacoin/poolhandlers.py
@@ -13,65 +13,6 @@ from coincurve import PrivateKey, PublicKey
 from yadacoin.config import Config
 
 
-class PoolHandler(BaseHandler):
-
-    async def get(self):
-        if self.config.mp is None:
-            self.config.mp = MiningPool()
-            #await self.config.mp.refresh()
-        """
-        if not self.config.mp.block_factory:
-            # first init
-            self.config.mp.refresh()
-        self.mining_index = self.config.mp.block_factory.block.index
-        
-        block = await self.config.mongo.async_db.blocks.find_one(sort=[('index',-1)])
-        # No need to run a query, this is cached in config object:
-        # self.config.BU.get_latest_block()['index']:
-        if self.config.mp.block_factory.block.index <= block['index']:
-            # We're behind
-            self.config.mp.refresh()
-        """
-        # Since self.config.mp is updated by the inner events as soon as possible, no need to refresh anything, it always has the latest block.
-        self.render_as_json(await self.config.mp.block_to_mine_info())
-
-
-class PoolSubmitHandler(BaseHandler):
-
-    async def post(self):
-        block_info = json.loads(self.request.body.decode('utf-8'))
-        data = block_info["nonce"]
-        address = block_info["address"]
-        if type(data) is not str:
-            self.render_as_json({'n':'Ko'})
-            return
-        if len(data) > CHAIN.MAX_NONCE_LEN:
-            self.render_as_json({'n': 'Ko'})
-            return
-        result = await self.config.mp.on_miner_nonce(data, address=address)
-        if result:
-            self.render_as_json({'n': 'ok'})
-        else:
-            self.render_as_json({'n': 'ko'})
-
-
-class PoolExplorer(BaseHandler):
-
-    async def get(self):
-        query = {}
-        if self.get_argument("address", False):
-            query['address'] = self.get_argument("address")
-        if self.get_argument("index", False):
-            query['index'] = self.get_argument("index")
-        res = self.mongo.db.shares.find_one(query, {'_id': 0}, sort=[('index', -1)])
-        if res and query:
-            self.render('Pool address: <a href="https://yadacoin.io/explorer?term=%s" target="_blank">%s</a>, Latest block height share: %s'
-                        % (self.config.address, self.config.address, res.get('index')))
-        else:
-            self.render('Pool address: <a href="https://yadacoin.io/explorer?term=%s" target="_blank">%s</a>, No history'
-                        % (self.config.address, self.config.address))
-
-
 class JSONRPC(BaseHandler):
     async def post(self):
         body = json.loads(self.request.body.decode())
@@ -169,7 +110,4 @@ class JSONRPC(BaseHandler):
 
 POOL_HANDLERS = [
     (r'/json_rpc', JSONRPC),
-    (r'/pool', PoolHandler),
-    (r'/pool-submit', PoolSubmitHandler),
-    (r'/pool-explorer', PoolExplorer)
 ]
