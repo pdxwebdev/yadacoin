eb00ca49b3954a35b1386f0f65c769e18eca45f2
Matt Vogel <matt@portlandwebdev.com>
2020-12-14 22:44:21 -0800

    make version report git version, add GitPython dep

--- yadacoin/__init__.py
+++ yadacoin/__init__.py
@@ -1,1 +1,5 @@
-version = (4, 0, 1)
+import git
+repo = git.Repo('.')
+tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
+latest_tag = tags[-1]
+version = tuple(str(latest_tag)[1:].split('.'))
