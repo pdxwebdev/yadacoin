39458ba862df0525be620ad30a9d8cb8514b7805
Matt Vogel <matt@portlandwebdev.com>
2021-04-02 18:04:34 -0700

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 # Install MongoDB
 sudo mkdir /data/db -p
 sudo chmod 777 /data/db
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt install -y gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 sudo apt update
 sudo apt install -y mongodb-org
 # Setup and start DB service
