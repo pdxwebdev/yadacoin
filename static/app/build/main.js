9ad2f53abfec3c4c60367737d0b9005dd2c1cb2d
Matt Vogel <matt@portlandwebdev.com>
2021-11-14 18:14:35 -0800

    update wallet app

--- static/app/build/main.js
+++ static/app/build/main.js
@@ -7646,117 +7646,9 @@ var ListPage = /** @class */ (function () {
         "collection": "group"
     },
     {
-        "username": "Welcome",
-        "username_signature": "MEUCIQCRL9/VTWYPkMRLOJ1X2n48eDyejyCNqGf7gkhFXs1Y2wIgO55/2x1H7Ar61Jpn0efI8fjilHrSNHahv+bcqAi+s90=",
-        "public_key": "02fe29f80c6ce222f4bb35c96b5d07fa1f6dddab7c6577ab34864e41c9fced7bb9",
-        "parent": {
-            "username": "Text Channels",
-            "username_signature": "MEQCIBnBj2TTVH7nNCIOsJxbhyKTKl/tMYErBwHnDVGEHlPAAiA2scfrrughyVpwfbzHXrGy0mv2yMFibthFjgqGiFwBOw==",
-            "public_key": "035d0535af1c3f419340b7ca275a804b355f91aad8984544c3ee10d0d773154496",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "General",
-        "username_signature": "MEQCIDjY3NT74HdGXC/evljwbJBcS01isNAi8APxhe5e97FSAiA/a0pgs63xnKkDdynbHfrkxpc2teVItb+YUV4K9xGrKQ==",
-        "public_key": "02709acb632e9a53bd0adc36f31ebcb96182f9acfb34fbefc5e37fed076028b835",
-        "parent": {
-            "username": "Text Channels",
-            "username_signature": "MEQCIBnBj2TTVH7nNCIOsJxbhyKTKl/tMYErBwHnDVGEHlPAAiA2scfrrughyVpwfbzHXrGy0mv2yMFibthFjgqGiFwBOw==",
-            "public_key": "035d0535af1c3f419340b7ca275a804b355f91aad8984544c3ee10d0d773154496",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Branding",
-        "username_signature": "MEQCIFR43ISmiTqmQfFdWCk6pIePCTFGWxN7P9/6WTGkcNPbAiBWRSz1rxSxJL6K6Yrn5pl67TUUc669mUZ3WgB9XI3GKw==",
-        "public_key": "036e63ee09992924d8822015054c70d5e985bb5d122ff035b875f8d30b5ce6d625",
-        "parent": {
-            "username": "Text Channels",
-            "username_signature": "MEQCIBnBj2TTVH7nNCIOsJxbhyKTKl/tMYErBwHnDVGEHlPAAiA2scfrrughyVpwfbzHXrGy0mv2yMFibthFjgqGiFwBOw==",
-            "public_key": "035d0535af1c3f419340b7ca275a804b355f91aad8984544c3ee10d0d773154496",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Announcements",
-        "username_signature": "MEUCIQCpU7ITz+n8xmkJXGI00hYPC7NAoPifePGKB7X9syxJaQIgRdCAEwWOMqgBXWLR8wa6QBfXy7STVT0EqMu79qJ8m18=",
-        "public_key": "02f9500e322fc15156e5cef8976e7518a5dc5fca1b00ca181b9cc7ab8e5252caf0",
-        "parent": {
-            "username": "Text Channels",
-            "username_signature": "MEQCIBnBj2TTVH7nNCIOsJxbhyKTKl/tMYErBwHnDVGEHlPAAiA2scfrrughyVpwfbzHXrGy0mv2yMFibthFjgqGiFwBOw==",
-            "public_key": "035d0535af1c3f419340b7ca275a804b355f91aad8984544c3ee10d0d773154496",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Market Research",
-        "username_signature": "MEUCIQD5ijB1NERJySjleus5Wyh0i4sQT+2EMUrGbVkth/QzMQIgE1GptX9am+qNaAZb2w2+QL85xKCxRtbVEb+daiPKOaY=",
-        "public_key": "032241a684e89d1f54372e501390aaf3eed941300d4b6a787797051cfc92a7ae68",
-        "parent": {
-            "username": "Text Channels",
-            "username_signature": "MEQCIBnBj2TTVH7nNCIOsJxbhyKTKl/tMYErBwHnDVGEHlPAAiA2scfrrughyVpwfbzHXrGy0mv2yMFibthFjgqGiFwBOw==",
-            "public_key": "035d0535af1c3f419340b7ca275a804b355f91aad8984544c3ee10d0d773154496",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Contributors",
-        "username_signature": "MEUCIQCY/sOB70VPct0Z9jSOwSjk+1wpBsfzGdj7dooMG63nCwIgRGQHVM6yQ3P9EejfIpPz5QAUPqpakSleOJLSGNsjdyc=",
-        "public_key": "02fe3ffbc6047672a9a8dc81184979ff606b5a0bd2fb3849f0a34fad61b769528b",
-        "parent": {
-            "username": "Text Channels",
-            "username_signature": "MEQCIBnBj2TTVH7nNCIOsJxbhyKTKl/tMYErBwHnDVGEHlPAAiA2scfrrughyVpwfbzHXrGy0mv2yMFibthFjgqGiFwBOw==",
-            "public_key": "035d0535af1c3f419340b7ca275a804b355f91aad8984544c3ee10d0d773154496",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
         "username": "Yada Protocol",
         "username_signature": "MEQCIEaKMo8CUL2gib2xj/vlIWSHPGuI2wfSXB8WLCb2saStAiAvIvALHRsRJjRfzXbkmyXNUIHF/tjcXUXtrTU1fO4bNQ==",
         "public_key": "02c757def602fa906e4d2b776cb0bceeb59683ffb1640ffd3c9ba6fd5378fa833f",
-        "collection": "group"
-    },
-    {
-        "username": "Indie App Devs",
-        "username_signature": "MEUCIQDghTs+/Sn4WE8ag/D8ZSbbPbLswDpIOjCE9iSEsHkTngIgfjSDq0c1PntYt9nDWCLbtMA95lqYRDO4GdyYgVvMsfQ=",
-        "public_key": "033bbd1513ff892b2e995d497dc5124093ca91d3e417cbd8d5af7a0040a7ffe787",
-        "parent": {
-            "username": "Yada Protocol",
-            "username_signature": "MEQCIEaKMo8CUL2gib2xj/vlIWSHPGuI2wfSXB8WLCb2saStAiAvIvALHRsRJjRfzXbkmyXNUIHF/tjcXUXtrTU1fO4bNQ==",
-            "public_key": "02c757def602fa906e4d2b776cb0bceeb59683ffb1640ffd3c9ba6fd5378fa833f",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Core Contributors",
-        "username_signature": "MEUCIQD0+6P6qf6nOalf8uQ78ubOQwfO+moEyfhF1+1gffftlAIgNW9Q4O6NOYt2eTtPj++xVsdBDrJtOLYWguWvLlB8Uzw=",
-        "public_key": "02319e7d19fe31719811041eac348aec2db0555eb67909c82a40af6817521768f2",
-        "parent": {
-            "username": "Yada Protocol",
-            "username_signature": "MEQCIEaKMo8CUL2gib2xj/vlIWSHPGuI2wfSXB8WLCb2saStAiAvIvALHRsRJjRfzXbkmyXNUIHF/tjcXUXtrTU1fO4bNQ==",
-            "public_key": "02c757def602fa906e4d2b776cb0bceeb59683ffb1640ffd3c9ba6fd5378fa833f",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Mobile Contributors",
-        "username_signature": "MEQCIGIEVACLtNuDV9hBl9iZaomxmxDWPS4CScGTqgnKmhPYAiAj6QCpb1RIyHSpLcyqIfFWZmG2KQReQfuk81iBDSV/VA==",
-        "public_key": "0254bc5f06af01843036df34bf4372716a4945cad1d74a31efc2d738e7f733deca",
-        "parent": {
-            "username": "Yada Protocol",
-            "username_signature": "MEQCIEaKMo8CUL2gib2xj/vlIWSHPGuI2wfSXB8WLCb2saStAiAvIvALHRsRJjRfzXbkmyXNUIHF/tjcXUXtrTU1fO4bNQ==",
-            "public_key": "02c757def602fa906e4d2b776cb0bceeb59683ffb1640ffd3c9ba6fd5378fa833f",
-            "collection": "group"
-        },
         "collection": "group"
     },
     {
@@ -7766,95 +7658,11 @@ var ListPage = /** @class */ (function () {
         "collection": "group"
     },
     {
-        "username": "iOS App Help",
-        "username_signature": "MEUCIQD5Ajr/iWHMH/aXZuSswNiakmiEQtFePolOvjYRw7MoAgIgQotCfGcF8srW3Q3f+ewTiHnFqv8ns6pAJbbW6RMyIWk=",
-        "public_key": "0357fcb43e01d5faf979df54f35d8411de6691051f8fd754529c00898488375c55",
-        "parent": {
-            "username": "Yada App",
-            "username_signature": "MEQCIEsBxY9EhC9SsVnaqCpckOFDRGB5yMuj6A9Lq6EhvrHBAiAZZwFZw6XIk067OFUWMMSPhLmnUowRpshlazWOSES/3w==",
-            "public_key": "03e103f6d5c43cc1d14de00710d7369dbdf7f8885f4e909b384a64bfd272dab506",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Android App Help",
-        "username_signature": "MEUCIQCuDyan7ys69D1Gx0nW9gUaeuvRzUWb1yHV/mvdW7E6aAIgEjxOaFiytDrnExhMlFiX4f6LRW+YQFdV2WiF3JXkAnc=",
-        "public_key": "03e5a23a9c6ec5e0f609c7b67a853c9bf5b773de1a02eef5f012de9289f8f20066",
-        "parent": {
-            "username": "Yada App",
-            "username_signature": "MEQCIEsBxY9EhC9SsVnaqCpckOFDRGB5yMuj6A9Lq6EhvrHBAiAZZwFZw6XIk067OFUWMMSPhLmnUowRpshlazWOSES/3w==",
-            "public_key": "03e103f6d5c43cc1d14de00710d7369dbdf7f8885f4e909b384a64bfd272dab506",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Web App Help",
-        "username_signature": "MEQCIADEwG8RThuy/8a+2RCipP+46qzvlZTFcW0rAJXbqU8EAiAzy5Vv3cqkSFyj2Yv1dhaPq17cQtv5iLuTHRKK7gj3Ew==",
-        "public_key": "03685d90aa1ceb182b26835467c157be0fbd772baf74370e7d09897abc9968e384",
-        "parent": {
-            "username": "Yada App",
-            "username_signature": "MEQCIEsBxY9EhC9SsVnaqCpckOFDRGB5yMuj6A9Lq6EhvrHBAiAZZwFZw6XIk067OFUWMMSPhLmnUowRpshlazWOSES/3w==",
-            "public_key": "03e103f6d5c43cc1d14de00710d7369dbdf7f8885f4e909b384a64bfd272dab506",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
         "username": "YadaCoin",
         "username_signature": "MEQCID2Kio1OSNG2pIB/8id8G8yo+Vfuj2rAB2K3BurAyTeTAiAQ3Wmd4ayXtxrgQGRBaFfKuZGxb2DVIwy0UY0LARG4cQ==",
         "public_key": "03cebd3b8b38d28a4d032b38e3aadc3f8a06e78c27f1fe06b73ac7bb682f03949e",
         "collection": "group"
     },
-    {
-        "username": "Wallet",
-        "username_signature": "MEQCIDhv30BfvY/6SBZ8H2rtXT9lXotuWYwSduNHuRk38Jm3AiA1HBB2+tlGHVVeUhAfaRZ5KxyVlWFmWUIWObSwcwLM8Q==",
-        "public_key": "02e11fb36ec55d1311c18526c504b42a9d3df66b9f66e32e4c8e8a2d499b2984be",
-        "parent": {
-            "username": "YadaCoin",
-            "username_signature": "MEQCID2Kio1OSNG2pIB/8id8G8yo+Vfuj2rAB2K3BurAyTeTAiAQ3Wmd4ayXtxrgQGRBaFfKuZGxb2DVIwy0UY0LARG4cQ==",
-            "public_key": "03cebd3b8b38d28a4d032b38e3aadc3f8a06e78c27f1fe06b73ac7bb682f03949e",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Mining",
-        "username_signature": "MEUCIQCHNJIV0ud3tbcYyw6l6vWBl/ku6GKdPG7NmxuzHoSluAIgbVWZvyOTR3xe+pjTgRnWsgpZhZkK4gV/OUH/hrigiTk=",
-        "public_key": "027f6b67c5261bd2968741a561a268ef69678dda5cae18ecf9fac462da17385bbc",
-        "parent": {
-            "username": "YadaCoin",
-            "username_signature": "MEQCID2Kio1OSNG2pIB/8id8G8yo+Vfuj2rAB2K3BurAyTeTAiAQ3Wmd4ayXtxrgQGRBaFfKuZGxb2DVIwy0UY0LARG4cQ==",
-            "public_key": "03cebd3b8b38d28a4d032b38e3aadc3f8a06e78c27f1fe06b73ac7bb682f03949e",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Mining Help",
-        "username_signature": "MEQCIFdUtE++3fv0UpYzM5R91nqDLlUar5ibPeGQk8+0fWk7AiAnv/3uwYIOeDFkQ2DJwzTXe5v7Cf1yJ5cbeAyHG9YIBw==",
-        "public_key": "02827f4036f3a193362f7d53a36e39e6a1ccc5042a81f5d5a43e2038878e702fcf",
-        "parent": {
-            "username": "YadaCoin",
-            "username_signature": "MEQCID2Kio1OSNG2pIB/8id8G8yo+Vfuj2rAB2K3BurAyTeTAiAQ3Wmd4ayXtxrgQGRBaFfKuZGxb2DVIwy0UY0LARG4cQ==",
-            "public_key": "03cebd3b8b38d28a4d032b38e3aadc3f8a06e78c27f1fe06b73ac7bb682f03949e",
-            "collection": "group"
-        },
-        "collection": "group"
-    },
-    {
-        "username": "Trading",
-        "username_signature": "MEQCIAYZHdjYySoMs5DhmfvobjoN5OmYFqesbDp/RmsnTmQLAiB248IxwsqsaPD3ifcaKf/Z4SS7WmnKxOizB/gfPmD1ig==",
-        "public_key": "02543b33e4fa5b217fe346c25b03bb394056e344b409422015fe1aea030f522975",
-        "parent": {
-            "username": "YadaCoin",
-            "username_signature": "MEQCID2Kio1OSNG2pIB/8id8G8yo+Vfuj2rAB2K3BurAyTeTAiAQ3Wmd4ayXtxrgQGRBaFfKuZGxb2DVIwy0UY0LARG4cQ==",
-            "public_key": "03cebd3b8b38d28a4d032b38e3aadc3f8a06e78c27f1fe06b73ac7bb682f03949e",
-            "collection": "group"
-        },
-        "collection": "group"
-    }
 ]);
 //# sourceMappingURL=groups.js.map
 
