14f6ccd7f9958a3663307d90b3fcb2d9af0e5c06
Matt Vogel <matt@portlandwebdev.com>
2022-04-27 06:42:00 +0000

    fix version bug

--- yadacoin/__init__.py
+++ yadacoin/__init__.py
@@ -1,5 +1,6 @@
 import git
-repo = git.Repo('.')
+from pathlib import Path
+repo = git.Repo(Path(__file__).resolve().parent.parent, odbt=git.db.GitDB)
 tags = sorted(repo.tags, key=lambda t: t.commit.committed_datetime)
 latest_tag = tags[-1]
 version = tuple(str(latest_tag)[1:].split('.'))
