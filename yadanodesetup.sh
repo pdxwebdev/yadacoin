08db02c8b25c72babca30b0f1aa55ba0c08b19dc
Matthew Vogel <matt@portlandwebdev.com>
2023-08-27 16:18:46 -0700

    fix: docker setup script

--- yadanodesetup.sh
+++ yadanodesetup.sh
@@ -1,66 +1,55 @@
 #!/bin/bash
 
-# Ensure the script is run with superuser privileges
+# Ensure the script is run as root
 if [ "$EUID" -ne 0 ]; then
-    echo "Please run this script as root."
-    exit
+  echo "Please run this script as root."
+  exit
 fi
 
-# Step 0: Install git if it isn't already
-if ! command -v git &> /dev/null; then
-    echo "Git is not installed. Installing Git..."
-    sudo apt update
-    sudo apt install -y git
-else
-    echo "Git is already installed."
-fi
+# Install required packages
+apt update
+apt install -y python3 python3-venv python3-pip git cmake
 
-# Step 1: Clone the Yadacoin repository
-if [ ! -d "/etc/yadacoin" ]; then
-    echo "Cloning the Yadacoin repository..."
-    sudo git clone https://github.com/pdxwebdev/yadacoin /etc/yadacoin
-else
-    echo "/etc/yadacoin directory already exists."
-fi
+# Create the directory for your application
+APP_DIR="/etc/yadacoin"
+mkdir -p "$APP_DIR"
+cd "$APP_DIR"
 
-# Step 2: Install Docker
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed. Installing Docker..."
-    sudo apt update
-    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt update
-    sudo apt install -y docker-ce
-else
-    echo "Docker is already installed."
-fi
+# Clone the repository
+git clone https://github.com/pdxwebdev/yadacoin .
 
-# Step 3: Set up user permissions
-sudo usermod -aG docker $USER
-sudo chown $USER:$USER /var/run/docker.sock
-sudo chown -R $USER:$USER /etc/yadacoin
+# Create a virtual environment
+python3 -m venv venv
 
-# Step 4: Create and enable systemd service
-SERVICE_FILE_CONTENT=$(cat <<EOF
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install required packages
+pip install --no-cache-dir -r requirements.txt
+
+# Deactivate the virtual environment
+deactivate
+
+# Create a systemd service for the process manager
+SERVICE_FILE="/etc/systemd/system/yadanodemanager.service"
+cat << EOF > "$SERVICE_FILE"
 [Unit]
-Description=Yadanode Manager
+Description=YadaCoin Node Manager
+After=network.target
 
 [Service]
-ExecStart=/usr/bin/python3 /etc/yadacoin/yadanodemanager.py /etc/yadacoin/config/config.json
-WorkingDirectory=/etc/yadacoin
+User=root
+WorkingDirectory=$APP_DIR
+ExecStart=/usr/bin/python3 yadanodemanager.py --config config.json
 Restart=always
-User=$USER
-Group=$USER
 
 [Install]
 WantedBy=multi-user.target
 EOF
-)
 
-echo "${SERVICE_FILE_CONTENT}" > /etc/systemd/system/yadanodemanager.service
-sudo systemctl daemon-reload
-sudo systemctl enable yadanodemanager.service
-sudo systemctl start yadanodemanager.service
+# Reload systemd and start the service
+systemctl daemon-reload
+systemctl enable yadanodemanager
+systemctl start yadanodemanager
 
-echo "Setup complete. The Yadacoin Node Manager is now running."
+echo "Setup complete. YadaCoin Node Manager is now running."
