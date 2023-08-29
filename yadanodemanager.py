9dbf10324615ee377ea7f6cf59aa4536cc4a6a0b
Matthew Vogel <matt@portlandwebdev.com>
2023-08-29 01:47:23 -0700

    fix: yada node manager

--- yadanodemanager.py
+++ yadanodemanager.py
@@ -1,33 +1,41 @@
 import os
 import subprocess
 import time
-import uuid
 
 
 class YadaNodeManager:
     def __init__(self):
         self.repo_path = "/etc/yadacoin"
-        self.base_project_name = "yadanodemanager"  # Base project name.
-        self.project_name = self.generate_project_name()  # Generate project name once
         self.service_name = "yada-node"
         self.update_interval_seconds = 3600
 
-    def generate_project_name(self):
-        unique_id = uuid.uuid4().hex
-        return f"{self.base_project_name}_{unique_id}"
-
-    def is_project_running(self):
+    def stop_previous_containers(self):
         try:
             output = (
                 subprocess.check_output(
+                    ["docker", "ps", "-aq", "-f", f"name={self.service_name}"]
+                )
+                .decode()
+                .strip()
+            )
+            if output:
+                subprocess.run(
                     [
-                        "docker-compose",
-                        "-p",
-                        self.project_name,
-                        "ps",
-                        "-q",
-                        self.service_name,
-                    ]
+                        "docker",
+                        "rm",
+                        "-f",
+                        output,
+                    ],  # Stop and remove previous containers
+                    cwd=self.repo_path,
+                )
+        except Exception as e:
+            print(f"Error while stopping previous containers: {e}")
+
+    def ensure_container_running(self):
+        try:
+            output = (
+                subprocess.check_output(
+                    ["docker", "ps", "-q", "-f", f"name={self.service_name}"]
                 )
                 .decode()
                 .strip()
@@ -54,63 +62,34 @@ class YadaNodeManager:
         )
         return original_commit != latest_commit
 
-    def docker_compose_up(self):
-        subprocess.run(
-            ["docker-compose", "-p", self.project_name, "up", "-d", self.service_name],
-            cwd=self.repo_path,
-        )
-
     def rebuild_docker_image(self):
         subprocess.run(
-            ["docker-compose", "-p", self.project_name, "down", "yada-node"],
+            ["docker-compose", "down", self.service_name],
             cwd=self.repo_path,
         )
         subprocess.run(
-            ["docker-compose", "-p", self.project_name, "build", "yada-node"],
+            ["docker-compose", "build", self.service_name],
             cwd=self.repo_path,
         )
         subprocess.run(
-            ["docker-compose", "-p", self.project_name, "up", "-d", "yada-node"],
+            ["docker-compose", "up", "-d", self.service_name],
             cwd=self.repo_path,
         )
 
     def start_docker_image(self):
         subprocess.run(
-            ["docker-compose", "-p", self.project_name, "up", "-d", "yada-node"],
+            ["docker-compose", "up", "-d", self.service_name],
             cwd=self.repo_path,
         )
-
-    def ensure_project_running(self):
-        if not self.is_project_running():
-            print(f"Project {self.project_name} is not running. Starting it up...")
-            self.start_docker_image()
-
-    def stop_previous_containers(self):
-        try:
-            output = (
-                subprocess.check_output(
-                    ["docker", "ps", "-aq", "-f", f"name={self.project_name}"]
-                )
-                .decode()
-                .strip()
-            )
-            if output:
-                subprocess.run(
-                    [
-                        "docker",
-                        "rm",
-                        "-f",
-                        output,
-                    ],  # Stop and remove previous containers
-                    cwd=self.repo_path,
-                )
-        except Exception as e:
-            print(f"Error while stopping previous containers: {e}")
 
     def run(self):
         self.stop_previous_containers()  # Stop and remove previous containers
         while True:
-            self.ensure_project_running()
+            if not self.ensure_container_running():
+                print(
+                    f"Container {self.service_name} is not running. Starting it up..."
+                )
+                self.start_docker_image()
             if self.git_pull_latest():
                 print(
                     "Codebase updated. Rebuilding Docker image and restarting container..."
