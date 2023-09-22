1da566e2f176ea1ddbb7d7189e9246090f2d1504
Matthew Vogel <matt@portlandwebdev.com>
2023-09-22 02:53:37 -0700

    fix: refactor tests for config

--- tests/unittests/core/test_nodes.py
+++ tests/unittests/core/test_nodes.py
@@ -9,7 +9,7 @@ from ..test_setup import AsyncTestCase
 
 class TestBlock(AsyncTestCase):
     async def test_nodes_valid(self):
-        for i, seed_list in Seeds.NODES.items():
+        for i, seed_list in Seeds().NODES.items():
             for seed in seed_list:
                 result = verify_signature(
                     base64.b64decode(seed.identity.username_signature),
@@ -17,7 +17,7 @@ class TestBlock(AsyncTestCase):
                     bytes.fromhex(seed.identity.public_key),
                 )
                 self.assertTrue(result)
-        for i, seed_gateway_list in SeedGateways.NODES.items():
+        for i, seed_gateway_list in SeedGateways().NODES.items():
             for seed_gateway in seed_gateway_list:
                 result = verify_signature(
                     base64.b64decode(seed_gateway.identity.username_signature),
@@ -25,7 +25,7 @@ class TestBlock(AsyncTestCase):
                     bytes.fromhex(seed_gateway.identity.public_key),
                 )
                 self.assertTrue(result)
-        for i, service_provider_list in ServiceProviders.NODES.items():
+        for i, service_provider_list in ServiceProviders().NODES.items():
             for service_provider in service_provider_list:
                 result = verify_signature(
                     base64.b64decode(service_provider.identity.username_signature),
