b2de52701c11a838f034512e0876185d58d7d5c6
Matthew Vogel <matt@portlandwebdev.com>
2023-09-22 18:23:40 -0700

    fix: remove test handler

--- yadacoin/http/node.py
+++ yadacoin/http/node.py
@@ -281,5 +281,4 @@ NODE_HANDLERS = [
     (r"/get-expired-smart-contract-transactions", GetExpiredSmartContractTransactions),
     (r"/get-expired-smart-contract-transaction", GetExpiredSmartContractTransaction),
     (r"/get-trigger-transactions", GetSmartContractTriggerTransaction),
-    (r"/test-timeout", TestTimeoutHandler),
 ]
