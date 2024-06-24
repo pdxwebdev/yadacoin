514bffc20a15955823dc16e562aed62be2a7b3e4
Matthew Vogel <matt@portlandwebdev.com>
2024-06-24 00:04:04 -0700

    feat: enhance explorer UI

--- plugins/explorer/src/app/search-form/search-form.component.ts
+++ plugins/explorer/src/app/search-form/search-form.component.ts
@@ -1,82 +1,99 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
-import { Search } from '../search';
+import { Component, OnInit } from "@angular/core";
+import { Http } from "@angular/http";
+import { Search } from "../search";
+
+declare var Bitcoin;
 
 @Component({
-  selector: 'app-search-form',
-  templateUrl: './search-form.component.html',
-  styleUrls: ['./search-form.component.css']
+  selector: "app-search-form",
+  templateUrl: "./search-form.component.html",
+  styleUrls: ["./search-form.component.css"],
 })
 export class SearchFormComponent implements OnInit {
-
-  model = new Search('');
+  model = new Search("");
   result = [];
-  resultType = '';
+  resultType = "";
   balance = 0;
   searching = false;
   submitted = false;
-  current_height = '';
-  circulating = '';
-  hashrate = '';
-  difficulty = '';
+  current_height = "";
+  circulating = "";
+  hashrate = "";
+  difficulty = "";
+  expertMode = false;
 
-  constructor(
-  	public http: Http
-  ) { 
-    this.http.get('/api-stats')
-    .subscribe((res: any) => {
-      this.difficulty = this.numberWithCommas(res.json()['stats']['difficulty']);
-      this.hashrate = this.numberWithCommas(res.json()['stats']['network_hash_rate']);
-      this.current_height = this.numberWithCommas(res.json()['stats']['height']);
-      this.circulating = this.numberWithCommas(res.json()['stats']['circulating']);
-      if (!window.location.search) {
-        this.http.get('/explorer-search?term=' + this.current_height.replace(',', ''))
-        .subscribe((res: any) => {
-          this.result = res.json().result || []
-          this.resultType = res.json().resultType
-          this.balance = res.json().balance
-          this.searching = false;
-        },
-        (err: any) => {
-          alert('something went terribly wrong!')
-        });
+  constructor(public http: Http) {
+    this.http.get("/api-stats").subscribe(
+      (res: any) => {
+        this.difficulty = this.numberWithCommas(
+          res.json()["stats"]["difficulty"]
+        );
+        this.hashrate = this.numberWithCommas(
+          res.json()["stats"]["network_hash_rate"]
+        );
+        this.current_height = this.numberWithCommas(
+          res.json()["stats"]["height"]
+        );
+        this.circulating = this.numberWithCommas(
+          res.json()["stats"]["circulating"]
+        );
+        if (!window.location.search) {
+          this.http
+            .get(
+              "/explorer-search?term=" + this.current_height.replace(",", "")
+            )
+            .subscribe(
+              (res: any) => {
+                this.result = res.json().result || [];
+                this.resultType = res.json().resultType;
+                this.balance = res.json().balance;
+                this.searching = false;
+              },
+              (err: any) => {
+                alert("something went terribly wrong!");
+              }
+            );
+        }
+      },
+      (err: any) => {
+        alert("something went terribly wrong!");
       }
-    },
-    (err: any) => {
-      alert('something went terribly wrong!')
-    });  
+    );
     if (window.location.search) {
       this.searching = true;
       this.submitted = true;
-      this.http.get('/explorer-search' + window.location.search)
-      .subscribe((res: any) => {
-        this.result = res.json().result || []
-        this.resultType = res.json().resultType
-        this.balance = res.json().balance
-        this.searching = false;
-      },
-      (err: any) => {
-        alert('something went terribly wrong!')
-      });
-    }  
+      this.http.get("/explorer-search" + window.location.search).subscribe(
+        (res: any) => {
+          this.result = res.json().result || [];
+          this.resultType = res.json().resultType;
+          this.balance = res.json().balance;
+          this.searching = false;
+        },
+        (err: any) => {
+          alert("something went terribly wrong!");
+        }
+      );
+    }
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSubmit() { 
+  onSubmit() {
     this.searching = true;
-  	this.submitted = true;
-  	this.http.get('/explorer-search?term=' + encodeURIComponent(this.model.term))
-  	.subscribe((res: any) => {
-  		this.result = res.json().result || []
-  		this.resultType = res.json().resultType
-      this.balance = res.json().balance
-      this.searching = false;
-  	},
-  	(err: any) => {
-  		alert('something went terribly wrong!')
-  	});	
+    this.submitted = true;
+    this.http
+      .get("/explorer-search?term=" + encodeURIComponent(this.model.term))
+      .subscribe(
+        (res: any) => {
+          this.result = res.json().result || [];
+          this.resultType = res.json().resultType;
+          this.balance = res.json().balance;
+          this.searching = false;
+        },
+        (err: any) => {
+          alert("something went terribly wrong!");
+        }
+      );
   }
 
   numberWithCommas(x) {
@@ -84,5 +101,16 @@ export class SearchFormComponent implements OnInit {
   }
 
   // TODO: Remove this when we're done
-  get diagnostic() { return JSON.stringify(this.model); }
+  get diagnostic() {
+    return JSON.stringify(this.model);
+  }
+
+  encodeAddress(publicKeyHex) {
+    const bitcoin = Bitcoin;
+    const pubkey = Bitcoin.ECPubKey(
+      Bitcoin.convert.hexToBytes(publicKeyHex),
+      true
+    );
+    return pubkey.getAddress().toString();
+  }
 }
