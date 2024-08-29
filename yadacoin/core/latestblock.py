c025e45cbf4b684d7c4d441241b6f5b2bb82fb88
Rakni1988 <magik.gsm@gmail.com>
2024-08-29 17:36:04 +0200

    add getblocktemplate function in latestblock

--- yadacoin/core/latestblock.py
+++ yadacoin/core/latestblock.py
@@ -1,9 +1,16 @@
+import time
+
 from yadacoin.core.config import Config
+from yadacoin.core.chain import CHAIN
 
 
 class LatestBlock:
     config = None
     block = None
+    blocktemplate_target = None
+    blocktemplate_time = None
+    blocktemplate_index = None
+    blocktemplate_hash = None
 
     @classmethod
     async def set_config(cls):
@@ -38,3 +45,48 @@ class LatestBlock:
             return await Block.from_dict(block)
         else:
             cls.block = None
+
+    @classmethod
+    async def get_block_template(cls):
+        from yadacoin.core.block import Block
+        if not cls.block:
+            await cls.update_latest_block()
+
+        if cls.block:
+            try:
+                #cls.config.app_log.info(f"Block version: {cls.block.version}")
+                #cls.config.app_log.info(f"Block hash: {cls.block.hash}")
+                #cls.config.app_log.info(f"Block index: {cls.block.index}")
+                #cls.config.app_log.info(f"Block time: {cls.block.time}")
+                #cls.config.app_log.info(f"Block transactions: {cls.block.transactions}")
+
+                if (cls.block.hash != cls.blocktemplate_hash or cls.block.index != cls.blocktemplate_index):
+                    cls.blocktemplate_time = int(time.time())
+
+                    new_block = await Block.init_async(
+                        version=cls.block.version,
+                        block_time=cls.blocktemplate_time,
+                        block_index=cls.block.index + 1,
+                        prev_hash=cls.block.hash,
+                    )
+
+                    cls.blocktemplate_target = await CHAIN.get_target_10min(cls.block, new_block)
+
+                    cls.blocktemplate_index = cls.block.index
+                    cls.blocktemplate_hash = cls.block.hash
+
+                target_hex = hex(cls.blocktemplate_target)[2:].rjust(64, '0')
+
+                return {
+                    "version": cls.block.version,
+                    "prev_hash": cls.block.hash,
+                    "index": cls.block.index,
+                    "target": target_hex,
+                    "time": cls.blocktemplate_time
+                }
+            except Exception as e:
+                cls.config.app_log.error(f"Error calculating block template: {e}")
+                raise e
+        else:
+            cls.config.app_log.error("No block found when trying to get block template.")
+        return None
