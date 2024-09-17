aa1349c11f6d031d60957889679c189c6ca1c99f
Rakni1988 <67603180+Rakni1988@users.noreply.github.com>
2024-09-17 11:19:31 +0200

    Update block.py

--- yadacoin/core/block.py
+++ yadacoin/core/block.py
@@ -206,7 +206,7 @@ class Block(object):
 
         for x in transactions:
             x = Transaction.ensure_instance(x)
-            if await x.is_contract_generated():
+            if await x.contract_generated:
                 generated_txns.append(x)
             else:
                 regular_txns.append(x)
@@ -609,7 +609,7 @@ class Block(object):
                 else:
                     for output in txn.outputs:
                         coinbase_sum += float(output.value)
-            elif txn.contract_generated:
+            elif await txn.contract_generated:
                 if self.index >= CHAIN.TXN_V3_FORK_CHECK_MINER_SIGNATURE:
                     result = verify_signature(
                         base64.b64decode(txn.miner_signature),
