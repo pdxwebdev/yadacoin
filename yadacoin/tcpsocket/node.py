c39fa98479c19d05a853fc9a635573d84c94a703
Matthew Vogel <matt@portlandwebdev.com>
2025-06-25 17:53:27 -0700

    fix: newtxn use mempool when verifying txns

--- yadacoin/tcpsocket/node.py
+++ yadacoin/tcpsocket/node.py
@@ -346,6 +346,7 @@ class NodeRPC(BaseRPC):
                 check_max_inputs=check_max_inputs,
                 check_masternode_fee=check_masternode_fee,
                 check_kel=check_kel,
+                mempool=True,
             )
         except MissingInputTransactionException:
             self.config.app_log.warning(
