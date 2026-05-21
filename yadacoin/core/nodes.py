088554a9c653eecc2b8da77464f69b3af09202d0
Matthew Vogel <matt@portlandwebdev.com>
2026-05-21 12:19:20 -0700

    fix: node fork point not considered

--- yadacoin/core/nodes.py
+++ yadacoin/core/nodes.py
@@ -29,6 +29,8 @@ class Nodes:
         for NODE in cls()._NODES:
             for rng in NODE["ranges"]:
                 cls().fork_points.append(rng[0])
+                if rng[1]:
+                    cls().fork_points.append(rng[1])
         cls().fork_points = sorted(list(set(cls().fork_points)))
 
     @classmethod
