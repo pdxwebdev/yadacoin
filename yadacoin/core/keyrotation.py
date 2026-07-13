abc56dd96b311b68b48fd2a1c6d82b785904e79e
Matt Vogel <matt@portlandwebdev.com>
2026-07-12 19:30:09 -0700

    fix: node identity resolution

--- yadacoin/core/keyrotation.py
+++ yadacoin/core/keyrotation.py
@@ -698,22 +698,21 @@ class NodeKeyRotationManager:
             cur_priv_obj = _CoincurvePrivateKey(cur["private_key"])
             cur_pub_bytes = cur_priv_obj.public_key.format(compressed=True)
             cur_address = str(P2PKHBitcoinAddress.from_pubkey(cur_pub_bytes))
-            if ke.prerotated_key_hash != cur_address:
-                config.app_log.info(f"{ke.twice_prerotated_key_hash} != {cur_address}")
-                config.app_log.info(f"{ke.prerotated_key_hash} != {cur_address}")
-                config.app_log.info(f"{ke.public_key_hash} != {cur_address}")
-                for _ in range(200):
-                    cur = derive_secure_path(
-                        cur["private_key"], cur["chain_code"], second_factor
-                    )
+            config.app_log.info(f"{ke.twice_prerotated_key_hash} != {cur_address}")
+            config.app_log.info(f"{ke.prerotated_key_hash} != {cur_address}")
+            config.app_log.info(f"{ke.public_key_hash} != {cur_address}")
+            while ke.prerotated_key_hash != cur_address:
+                cur = derive_secure_path(
+                    cur["private_key"], cur["chain_code"], second_factor
+                )
 
-                    cur_priv_obj = _CoincurvePrivateKey(cur["private_key"])
-                    cur_pub_bytes = cur_priv_obj.public_key.format(compressed=True)
-                    cur_address = str(P2PKHBitcoinAddress.from_pubkey(cur_pub_bytes))
-                    if ke.prerotated_key_hash == cur_address:
-                        break
-                else:
-                    return
+                cur_priv_obj = _CoincurvePrivateKey(cur["private_key"])
+                cur_pub_bytes = cur_priv_obj.public_key.format(compressed=True)
+                cur_address = str(P2PKHBitcoinAddress.from_pubkey(cur_pub_bytes))
+                if ke.prerotated_key_hash == cur_address:
+                    break
+            else:
+                return
         kn = cur  # UNCONFIRMED signer
 
         kn1 = derive_secure_path(kn["private_key"], kn["chain_code"], second_factor)
