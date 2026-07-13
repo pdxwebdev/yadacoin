216e633c9d84f7e17e8f2f8d2d93675d4e875427
Matt Vogel <matt@portlandwebdev.com>
2026-07-12 19:16:45 -0700

    fix: node identity resolution

--- yadacoin/core/keyrotation.py
+++ yadacoin/core/keyrotation.py
@@ -697,14 +697,11 @@ class NodeKeyRotationManager:
             )
             cur_priv_obj = _CoincurvePrivateKey(cur["private_key"])
             cur_pub_bytes = cur_priv_obj.public_key.format(compressed=True)
-            if ke.prerotated_key_hash != cur_pub_bytes.hex():
-                config.app_log.info(
-                    f"{ke.twice_prerotated_key_hash} != {cur_pub_bytes.hex()}"
-                )
-                config.app_log.info(
-                    f"{ke.prerotated_key_hash} != {cur_pub_bytes.hex()}"
-                )
-                config.app_log.info(f"{ke.public_key_hash} != {cur_pub_bytes.hex()}")
+            cur_address = str(P2PKHBitcoinAddress.from_pubkey(cur_pub_bytes))
+            if ke.prerotated_key_hash != cur_address:
+                config.app_log.info(f"{ke.twice_prerotated_key_hash} != {cur_address}")
+                config.app_log.info(f"{ke.prerotated_key_hash} != {cur_address}")
+                config.app_log.info(f"{ke.public_key_hash} != {cur_address}")
                 for _ in range(200):
                     cur = derive_secure_path(
                         cur["private_key"], cur["chain_code"], second_factor
