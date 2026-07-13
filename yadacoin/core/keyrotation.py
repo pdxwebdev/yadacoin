9d8594b561b18ff45239dee83c7ce7661e5f3b74
Matt Vogel <matt@portlandwebdev.com>
2026-07-12 23:17:29 -0700

    fix: node identity resolution

--- yadacoin/core/keyrotation.py
+++ yadacoin/core/keyrotation.py
@@ -519,8 +519,8 @@ class NodeKeyRotationManager:
         prev_address = str(P2PKHBitcoinAddress.from_pubkey(prev_pub_bytes))
 
         if block:
-            block.public_key = prev_pub_hex.to_hex()
-            block.private_key = prev_key["private_key"]
+            block.public_key = prev_pub_hex
+            block.private_key = prev_key["private_key"].to_hex()
         # Derive next ratchet key (the "confirming" key)
         next_key = derive_secure_path(
             prev_key["private_key"], prev_key["chain_code"], second_factor
